e6d23ec816ec10569d192d5d5be4018ae4b56f11
krzyGa <gibalak1a@gmail.com>
2019-02-14 12:23:28 +0100

    Initial Commit: MVC Spring [chapter 3][Zrozumieć parametry żądania]

--- src/main/java/com/packt/webstore/controller/ProductController.java
+++ src/main/java/com/packt/webstore/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Map;
@@ -41,5 +42,11 @@ public class ProductController {
     public String getProfuctsByFilter(Model model, @MatrixVariable(pathVar = "ByCriteria")Map<String, List<String>> filterParams){
         model.addAttribute("products", productService.getProductsByFilter(filterParams));
         return "products";
+    }
+
+    @RequestMapping("/product")
+    public String getProductById(Model model, @RequestParam("id") String productId){
+        model.addAttribute(productService.getProductById(productId));
+        return "product";
     }
 }
